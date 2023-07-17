93df0483704002fd752ee5ebbd4bc3f4899d442f
vietvu69 <viet.vuanh69@gmail.com>
2023-07-17 09:39:27 +0700

    update backend

--- backend/views/layouts/login.php
+++ backend/views/layouts/login.php
@@ -29,7 +29,7 @@
                         <div class="d-flex align-items-center container p-0">
                             <div class="page-logo width-mobile-auto m-0 align-items-center justify-content-center p-0 bg-transparent bg-img-none shadow-0 height-9">
                                 <a href="javascript:void(0)" class="page-logo-link press-scale-down d-flex align-items-center">
-                                    <img src="/img/logo.svg" alt="<?= Yii::$app->name ?>" aria-roledescription="logo">
+                                    <img src="/img/logo.svg" alt="<?= Yii::$app->name ?>" aria-roledescription="logo" style="width:45px">
                                 </a>
                             </div>
                         </div>
